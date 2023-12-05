d3a66f683a1feb683d3d0de42fca2f03bf42ee8b
Anton Kling <anton@kling.gg>
2023-12-05 20:13:44 +0100

    Add part 1 for day 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,7 +1,36 @@
 use std::cmp;
 use std::fs;
 
-fn play_game(l: &str) -> i32 {
+fn play_part1(l: &str) -> bool {
+    let game_state = l.split(":").collect::<Vec<_>>();
+    for round in game_state.last().unwrap().split(";") {
+        let mut parsed_state: [i32; 3] = [0; 3];
+        for cubes in round.split(",") {
+            let t = cubes.split(" ").collect::<Vec<_>>();
+            let color = *t.last().unwrap();
+            let number = t[1].parse::<i32>().unwrap();
+            let index = match color {
+                "red" => 0,
+                "green" => 1,
+                "blue" => 2,
+                _ => {panic!("Invalid color")}
+            };
+            parsed_state[index] = number;
+        }
+        if parsed_state[0] > 12 {
+            return false;
+        }
+        if parsed_state[1] > 13 {
+            return false;
+        }
+        if parsed_state[2] > 14 {
+            return false;
+        }
+    }
+    true
+}
+
+fn play_part2(l: &str) -> i32 {
     let game_state = l.split(":").collect::<Vec<_>>();
     let mut result: [i32; 3] = [0; 3];
     for round in game_state.last().unwrap().split(";") {
@@ -29,10 +58,15 @@ fn play_game(l: &str) -> i32 {
 
 fn main() -> Result<(), std::io::Error> {
     let content = fs::read_to_string("./day_02/input.txt")?;
-    let mut sum = 0;
-    for l in content.lines() {
-        sum += play_game(l);
+    let mut part1 = 0;
+    let mut part2 = 0;
+    for (index, l) in content.lines().enumerate() {
+        if play_part1(l) {
+            part1+=index+1;
+        }
+        part2 += play_part2(l);
     }
-    println!("{}", sum);
+    println!("part1: {}", part1);
+    println!("part2: {}", part2);
     Ok(())
 }
